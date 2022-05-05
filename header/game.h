e679f5c60be0f027414288fac736dbd62c0f40de
Yunfan Jiang <103341315+JiangYunfan1999@users.noreply.github.com>
2022-05-05 11:40:58 +0800

    Update game.h

--- header/game.h
+++ header/game.h
@@ -1,1 +1,14 @@
+#define ROW 3
+#define COL 3
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//函数的声明
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
+//能返回四种状态：玩家赢'*'；电脑赢'#'；平局'Q'；游戏继续'C'
